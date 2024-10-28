30cde83a5d3ef60958cf1fc17adf4685aaf3e1bc
giovanni <giovi.5057@gmail.com>
2024-10-28 10:23:36 +0100

    risolto bug nel riconoscimento di sintassi errata negli argomenti di input

--- src/main/java/it/giovannigrieco/Main.java
+++ src/main/java/it/giovannigrieco/Main.java
@@ -17,7 +17,8 @@ public class Main {
 
         List<String> valuesFiles = new ArrayList<>();
         String templateFile = null;
-
+        boolean templateFlag = false;
+        boolean valuesFlag = false;
         for(int i = 0 ; i < args.length ; i++){
             String arg = args[i];
             if( arg.equals("--help") || arg.equals("-h") ){
@@ -30,7 +31,12 @@ public class Main {
             }
 
             if( arg.equals("--template") || arg.equals("-t") ){
+                templateFlag = true;
                 if( i + 1 < args.length ){
+                    if(args[i+1].equals("-v") || args[i+1].equals("--values")){
+                        System.out.println("Error: missing argument for option " + arg);
+                        System.exit(1);
+                    }
                     templateFile = args[i+1];
                     i++;
                 }else{
@@ -40,7 +46,12 @@ public class Main {
             }
 
             if( arg.equals("--values") || arg.equals("-v") ){
+                valuesFlag = true;
                 if( i + 1 < args.length ){
+                    if(args[i+1].equals("-t") || args[i+1].equals("--template")){
+                        System.out.println("Error: missing argument for option " + arg);
+                        System.exit(1);
+                    }
                     String[] values = args[i+1].split(",");
                     valuesFiles.addAll(Arrays.asList(values));
                     i++;
@@ -50,6 +61,16 @@ public class Main {
                 }
             }
 
+        }
+
+        if( !templateFlag ){
+            System.out.println("Error: missing template file");
+            System.exit(1);
+        }
+
+        if( !valuesFlag ){
+            System.out.println("Error: missing values files");
+            System.exit(1);
         }
 
         println("Template file: " + templateFile);
